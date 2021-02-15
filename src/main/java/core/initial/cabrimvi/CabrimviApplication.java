10183bcfd53d8dbc537b30a417514a923a396eb7
Marcu <Constantin-Adrian.Marcu@ing.com>
2021-02-15 23:33:39 +0200

    initial push to git

---
+++ src/main/java/core/initial/cabrimvi/CabrimviApplication.java
@@ -0,0 +1,13 @@
+package core.initial.cabrimvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabrimviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabrimviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/initial/cabrimvi/CabrimviApplicationTests.java
@@ -0,0 +1,13 @@
+package core.initial.cabrimvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabrimviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
